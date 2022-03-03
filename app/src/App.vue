a45722148d4d8ece7dbb38a745c4ac95785f0507
yusakuvol <yoursakuyu@gmail.com>
2022-03-03 21:03:04 +0900

    [Fix] #3 Fix files for new components
    [Fix] #3 Fix files for new components

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,3 +1,35 @@
 <template>
-  <div class="trade-modal">Hello dYdX!!!</div>
+  <div class="trade-modal">
+    <TradeHeader />
+    <TradeInfo />
+    <MarketOrder />
+    <LimitOrder />
+  </div>
 </template>
+
+<script lang="ts">
+import TradeHeader from "./components/TradeHeader.vue";
+import TradeInfo from "./components/TradeInfo.vue";
+import MarketOrder from "./components/MarketOrder.vue";
+import LimitOrder from "./components/LimitOrder.vue";
+
+export default {
+  name: "app",
+  components: {
+    TradeHeader,
+    TradeInfo,
+    MarketOrder,
+    LimitOrder,
+  },
+};
+</script>
+
+<style scoped>
+.trade-modal {
+  background-color: #000000;
+  color: #ffffff;
+  position: absolute;
+  width: 300px;
+  height: 800px;
+}
+</style>
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
 
 document
   ?.querySelector("body")
   ?.insertAdjacentHTML(`afterbegin`, `<div id="trade-extension"></div>`);
 
-createApp(App).use(store).use(router).mount("#trade-extension");
+createApp(App).use(store).mount("#trade-extension");
