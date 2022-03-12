fcbf45f0403408e78168131a11a99366ba337265
yusakuvol <yoursakuyu@gmail.com>
2022-03-12 22:34:13 +0900

    [Add]#3 Commit the TailwindCSS default file

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,7 @@ import store from "./store";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "./index.css";
 
 library.add(fas);
 document
