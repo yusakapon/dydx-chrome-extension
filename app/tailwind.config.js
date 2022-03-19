ce820a92013d208e4eea02f1d8cc5ccd36692588
yusakuvol <yoursakuyu@gmail.com>
2022-03-19 11:18:07 +0900

    [Fix] #9 add colors in tailwindcss

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        modal: "#1c1c28",
+        "modal-container": "#333347",
+        sell: "#ff5353",
+        buy: "#3fb68b",
+      },
+    },
   },
   plugins: [],
 };
