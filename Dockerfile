c0109df5d66d96d3485604d1b9798abf24b19e6c
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2022-05-02 13:32:33 +0900

    fix: スクリプトの調整

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,14 @@ FROM node:16-slim
 
 WORKDIR /app
 
-RUN apt-get update && apt-get -y install make gcc g++ python3 sudo
+RUN apt-get update && \
+    apt-get -y install \
+    make \
+    gcc \
+    g++ \
+    python3 \
+    sudo \
+    procps
 RUN npm install -g npm @vue/cli
 
 USER node
--- app/bin/adjust-dist.sh
+++ app/bin/adjust-dist.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# move dir
-cd ../
-
 # exec
 while :
 do
   sleep 30
-  if [ -e dist/index.html ]; then
+  if [ -d dist ] && [ -e dist/index.html ]; then
     cp src/manifest.json dist/
     cp src/loader.js dist/
     rm dist/index.html
     echo "Finish adjust-dist"
+    break
+  elif [ -e dist/manifest.json ] && [ -e dist/loader.js ]; then
+    echo "adjust-dist is already Finished"
     break
   else
     echo "Continue adjust-dist"
